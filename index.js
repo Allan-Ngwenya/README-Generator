bd298575edbbc3cc6312bcfc66d77f679384b574
Mbongeni Ngwenya <mbongeniallanngwenya@hotmail.co.uk>
2022-01-09 15:53:45 +0000

    inquirer will be used with prompts to get the input from users

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 function init() {
 
+
 	const inquirer = require('inquirer');
 	const fs = require('fs');
 	const generate = require('./utils/generateMarkdown.js');
 	const path = require('path');
 
-}
+
+	inquirer
+		.prompt([
+			{
+
+			}
